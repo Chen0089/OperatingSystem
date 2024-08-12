8c513a6b9024497cab2c487062dc6a721d25eff6
Chen0089 <108268190+13381512347@users.noreply.github.com>
2024-08-12 21:04:10 +0800

    Update Include.hh

--- Include.hh
+++ Include.hh
@@ -4,5 +4,10 @@
 #include <fstream>
 #include <string>
 #include <filesystem>
+#include <mshtml.h>
+#include <exdisp.h>
+#include <atlbase.h>
+#include <atlcom.h>
+#include <atlwin.h>
 
 using namespace std;
