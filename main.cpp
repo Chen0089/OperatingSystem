77fc1a1c5f57e73db9adad19519dd58013a7c23d
Chen0089 <108268190+13381512347@users.noreply.github.com>
2024-08-12 20:40:24 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include "Include.hh"
 
 int main() {
-    cout << "This system is empty.Please update :D";
+    // 文件名数组
+    const std::string files[] = { "file1.txt", "file2.txt", "file3.txt" };
+    size_t count = sizeof(files) / sizeof(files[0]);
+
+    // 遍历检查每个文件是否存在
+    for (size_t i = 0; i < count; ++i) {
+        if (std::filesystem::exists(files[i])) {
+            std::cout << "File exists: " << files[i] << std::endl;
+        } else {
+            std::cout << "File does not exist: " << files[i] << std::endl;
+        }
+    }
+
     return 0;
 }
