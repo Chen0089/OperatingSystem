49f9ac00155f58295f9f721f283259584ea929f0
Chen0089 <108268190+Chen0089@users.noreply.github.com>
2025-01-11 15:51:12 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,23 +5,28 @@ void run_bat_file(const string& bat_file) {
         cout << "正在执行: " << bat_file << endl;
 
         // 设置启动信息
-        STARTUPINFO si = {0};
-        PROCESS_INFORMATION pi = {0};
-        
+        STARTUPINFO si = { 0 };
+        PROCESS_INFORMATION pi = { 0 };
+
         si.cb = sizeof(si);
-        
+
+        // 将 std::string 转换为 std::wstring
+        int size_needed = MultiByteToWideChar(CP_UTF8, 0, bat_file.c_str(), (int)bat_file.size(), NULL, 0);
+        wstring wbat_file(size_needed, 0);
+        MultiByteToWideChar(CP_UTF8, 0, bat_file.c_str(), (int)bat_file.size(), &wbat_file[0], size_needed);
+
         // 创建进程
-        if (CreateProcess(
-                NULL, 
-                const_cast<char*>(bat_file.c_str()),  // BAT 文件路径
-                NULL, 
-                NULL, 
-                FALSE, 
-                0, 
-                NULL, 
-                NULL, 
-                &si, 
-                &pi)) 
+        if (CreateProcessW(
+            NULL,
+            const_cast<LPWSTR>(wbat_file.data()),  // BAT 文件路径
+            NULL,
+            NULL,
+            FALSE,
+            0,
+            NULL,
+            NULL,
+            &si,
+            &pi))
         {
             // 等待进程结束
             WaitForSingleObject(pi.hProcess, INFINITE);
@@ -31,30 +36,33 @@ void run_bat_file(const string& bat_file) {
             GetExitCodeProcess(pi.hProcess, &exitCode);
             if (exitCode == 0) {
                 cout << "执行成功！" << endl;
-            } else {
+            }
+            else {
                 cout << "执行失败，错误代码：" << exitCode << endl;
             }
 
             // 关闭进程和线程句柄
             CloseHandle(pi.hProcess);
             CloseHandle(pi.hThread);
-        } else {
+        }
+        else {
             cerr << "创建进程失败，错误代码：" << GetLastError() << endl;
         }
-    } else {
+    }
+    else {
         cout << "无效的文件格式！请提供一个 .bat 文件。" << endl;
     }
 }
 
 int main() {
-    cout << "最后更新：2025/01/09,如要更新，请前往chen0089.github.io/OperatingSystem" << endl
-         << "初始化中，请耐心等待..." << endl;
+    cout << "最后更新：2025/01/11,如要更新，请前往chen0089.github.io/OperatingSystem" << endl
+        << "初始化中，请耐心等待..." << endl;
 
     //初始化
     string command;
 
     cout << "初始化成功，命令行系统已启动。输入 'exit' 来退出。" << endl;
-    
+
     while (true) {
         cout << "请输入命令: ";
         getline(cin, command);
@@ -62,7 +70,8 @@ int main() {
         if (command == "exit") {
             cout << "退出命令行系统" << endl;
             break;
-        } else {
+        }
+        else {
             run_bat_file(command);
         }
     }
