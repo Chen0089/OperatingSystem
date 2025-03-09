44e69927477b4f5a23ee8199aa15295100e69a1b
Chen0089 <108268190+Chen0089@users.noreply.github.com>
2025-03-09 20:04:46 +0800

    增加了3个依赖项 1个加速 1个pragma

--- Include.hh
+++ Include.hh
@@ -1,3 +1,6 @@
+// 加速
+#pragma once
+
 // 标准库
 #include <iostream>
 #include <fstream>
@@ -8,8 +11,14 @@
 #include <ctime>
 #include <vector>
 #include <map>
+#include <wininet.h>
+#include <thread>
+#include <atomic>
 #include <algorithm>
 
 // 外部库
 #include "curl/curl.h"
 #include "nlohmann/json.hpp"
+
+// pragma
+#pragma comment(lib, "wininet.lib")
