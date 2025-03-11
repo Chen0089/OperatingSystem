3dbdb0c1496419e9cc8247e8f6b6b2314356de52
Chen0089 <108268190+Chen0089@users.noreply.github.com>
2025-03-11 21:09:19 +0800

    更新

--- Include.hh
+++ Include.hh
@@ -1,11 +1,16 @@
+// 谁还没有个加速哇
+#pragma once
+
 #include <windows.h>
 #include <gdiplus.h>
 #include <shlwapi.h>
-#include <algorithm>
-#include <random>
-#include <vector>
-
+#include <netlistmgr.h>  // 网络状态检测
+#include <wrl/client.h>  // ComPtr
+#include <string>
+#include <chrono>
 #pragma comment(lib, "gdiplus.lib")
 #pragma comment(lib, "shlwapi.lib")
+#pragma comment(lib, "ole32.lib")
 
 using namespace Gdiplus;
+using namespace Microsoft::WRL;
