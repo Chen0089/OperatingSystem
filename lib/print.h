b9a7db8a27a38010472282aaf5b4f29102a211af
Chen0089AltAccount <18049754322@163.com>
2026-03-28 13:39:34 +0000

    打印十进制和十六进制集成到了print所以删掉

--- lib/print.h
+++ lib/print.h
@@ -6,15 +6,6 @@
 // 打印字符串
 void print(const char* str);
 
-// 打印十六进制数
-void print_hex(uint32_t num);
-
-// 打印十进制数
-void print_dec(uint32_t num);
-
-// 打印整数（自动判断格式，简单实现）
-void print_int(int32_t num);
-
 // 带格式化的打印（简化版，仅支持 %s %x %d）
 void printf(const char* fmt, ...);
 
